51b5f24285fd5a230a44ea377752804ec9e9651a
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-12 22:15:07 +0600

    Update O_N.cpp

--- O_N.cpp
+++ O_N.cpp
@@ -9,11 +9,14 @@ int main()
     {
         cin>>a[i];
     }
+    
     int s=0;
     for(int i=0;i<n;i+=2) // O(N)
     {
         s+=a[i];
     }
+    
     cout<<s<<endl;
+    
     return 0;
 }
