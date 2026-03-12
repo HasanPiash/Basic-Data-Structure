5c8745a145914b6d1794154aab85c6e183e213e5
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-12 22:15:58 +0600

    Update O_N_logN.cpp

--- O_N_logN.cpp
+++ O_N_logN.cpp
@@ -4,15 +4,18 @@ int main()
 {
     int n;
     cin>>n;
+    
     for(int i=1;i<=n;i++) // O(Nlog(N))
     {
         int x=i;
+        
         while(x>0)  
         {
             int digit=x%10;
             cout<<digit<<" ";
             x/=10;
         }
+        
         cout<<endl;
     }
     return 0;
